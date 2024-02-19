8a813c067ec15e0a167c705a62e3bf36e9d3993c
Kavya Kumar <100834021+KavyakKumar@users.noreply.github.com>
2024-02-19 16:35:47 +0530

    Add files via upload

---
+++ frontend/day1/App.jsx
@@ -0,0 +1,13 @@
+// app.jsx
+// import React from 'react';
+import Login from './components/login';
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
