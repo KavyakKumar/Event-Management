a97ebbfcad9dc9edc840ea652177006a54388d2a
Kavya Kumar <100834021+KavyakKumar@users.noreply.github.com>
2024-02-23 23:11:25 +0530

    Add files via upload

---
+++ frontend/Day 2/Navbar.jsx
@@ -0,0 +1,42 @@
+import '../assets/styles.css';
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        {/* <img src="./assets/logo.png" alt="Logo" /> */}
+        <h2>Saphire Events</h2>
+      </div>
+      <ul className="nav-links">
+      <li><Link to="/HomePage">HomePage</Link></li>
+        <li><a href="Services">Services</a></li>
+        <li><a href="Events">Events</a></li>
+        <li><Link to="/Login">Login</Link></li>
+        <li><Link to="/Register">SignUp</Link></li>
+        <li><Link to="/AdminLogin">Admin</Link></li>
+        {/* <li><a href='/Login'  >Login</a> </li> */}
+
+      </ul>
+    </nav>
+
+    
+  );
+};
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Switch>
+//           <Route path='/Login' } />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
+
+export default Navbar;
+
+
